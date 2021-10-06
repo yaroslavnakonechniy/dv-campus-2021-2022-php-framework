d8beb208d2a304dd4fe182b5e8775bf49d6d71a0
{DV.Campus} <yaroslav.n@open-tag.com>
2021-10-06 11:19:57 +0300

    helloworld

--- web/hello-world.php
+++ web/hello-world.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Hello, World!</title>
+    <title>Hello, World! Yaroslav</title>
 </head>
 <body>
 <p style="font-size: 48px;">Hello, World!</p>
